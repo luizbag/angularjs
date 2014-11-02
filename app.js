7d01d850db1373a00f6f931b2db97c0f8f6356b1
Luiz Gavioli <luizbag@gmail.com>
2014-11-02 16:09:17 -0200

    Form validations

--- app.js
+++ app.js
@@ -52,4 +52,12 @@
       return this.tab === checkTab;
     };
   });
+
+  app.controller('ReviewController', function() {
+    this.review={}
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+      this.review={}
+    };
+  });
 })();
