ed38d6f7b74eab3e68fe1c1dfc9590d75e2cf85b
Luiz Gavioli <luizbag@gmail.com>
2014-11-02 12:12:18 -0200

    Tabs e novo controller

--- app.js
+++ app.js
@@ -11,19 +11,45 @@
       }
     ],
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    reviews: [
+      {
+        stars:5,
+        body: "I love this product!",
+        author:"joe@thomas.com"
+      }
+    ]
   },
   {
     name: "Pentagonal Gem",
     price: 5,
     description: "...",
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    reviews: [
+      {
+        stars:5,
+        body: "I love this product!",
+        author:"joe@thomas.com"
+      }
+    ]
   }];
 
   var app = angular.module('store', [ ]);
 
   app.controller('StoreController', function() {
     this.products = gems;
+  });
+
+  app.controller("PanelController", function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
   });
 })();
